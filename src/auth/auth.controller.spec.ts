0fb35c79238a4ff500e19f36dec46c9fb4ef53eb
Pedro de Castro Tedesco <pedrocastro.tedesco@gmail.com>
2024-09-10 10:56:25 -0300

    refactor[tests]: adição de testes básicos para auth service e auth controller. Adição de variáveis de ambiente no env.test

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,18 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { AuthModule } from './auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
 import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
 
-describe('AuthController', () => {
+describe('AuthService', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController],
-      providers: [AuthService],
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath: '.env.test',
+          isGlobal: true,
+        }),
+        MongooseModule.forRootAsync({
+          imports: [ConfigModule],
+          useFactory: async (configService: ConfigService) => ({
+            uri: configService.get<string>('MONGODB_URI_LOCAL'),
+          }),
+          inject: [ConfigService],
+        }),
+        AuthModule
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
   });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+  });
+
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,16 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { AuthModule } from './auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath: '.env.test',
+          isGlobal: true,
+        }),
+        MongooseModule.forRootAsync({
+          imports: [ConfigModule],
+          useFactory: async (configService: ConfigService) => ({
+            uri: configService.get<string>('MONGODB_URI_LOCAL'),
+          }),
+          inject: [ConfigService],
+        }),
+        AuthModule
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
   });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+  });
+
 
   it('should be defined', () => {
     expect(service).toBeDefined();
